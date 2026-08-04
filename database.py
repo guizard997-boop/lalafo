8d9ee997678a1b809a87e3aabd6d3851f6ccdafb
guizard997-boop <guizard997@gmail.com>
2026-08-04 19:03:32 +0600

    Update database.py

--- database.py
+++ database.py
@@ -1,34 +1,65 @@
-from sqlalchemy import create_engine, Column, Integer, String
+
+    from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-engine = create_engine("sqlite:///cars.db")
+
+engine = create_engine(
+    "sqlite:///cars.db"
+)
+
 
 Base = declarative_base()
 
 
 class Car(Base):
+
     __tablename__ = "cars"
 
-    id = Column(Integer, primary_key=True)
-    link = Column(String, unique=True)
-    title = Column(String)
-    price = Column(Integer)
+    id = Column(
+        Integer,
+        primary_key=True
+    )
+
+    link = Column(
+        String,
+        unique=True
+    )
+
+    title = Column(
+        String
+    )
+
+    price = Column(
+        Integer
+    )
+
 
 
 Base.metadata.create_all(engine)
 
-Session = sessionmaker(bind=engine)
+
+Session = sessionmaker(
+    bind=engine
+)
+
 
 
 def is_new(link):
+
     db = Session()
-    result = db.query(Car).filter_by(link=link).first()
+
+    car = db.query(Car).filter_by(
+        link=link
+    ).first()
+
     db.close()
 
-    return result is None
+    return car is None
+
 
 
 def save_car(link, title, price):
+
     db = Session()
 
     car = Car(
@@ -39,4 +70,5 @@ def save_car(link, title, price):
 
     db.add(car)
     db.commit()
+
     db.close()
