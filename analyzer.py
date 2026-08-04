2322e1ec617918e60ed75a8cbbd5b3b435e539ed
guizard997-boop <guizard997@gmail.com>
2026-08-04 19:02:47 +0600

    Update analyzer.

--- analyzer.py
+++ analyzer.py
@@ -1,50 +1,22 @@
-def calculate_market_price(cars):
-
-    """
-    cars - список цен похожих машин
-    """
-
-    if len(cars) == 0:
-        return 0
-
-
-    cars = sorted(cars)
-
-
-    middle = len(cars) // 2
-
-
-    return cars[middle]
-
-
-
 def discount_percent(price, market_price):
 
     if market_price == 0:
         return 0
 
-
-    result = (
+    discount = (
         (market_price - price)
         /
         market_price
     ) * 100
 
-
-    return round(result, 1)
+    return round(discount, 1)
 
 
 
 def is_good_offer(price, market_price):
 
-    discount = discount_percent(
+    return discount_percent(
         price,
         market_price
-    )
-
-
-    if discount >= 15:
-        return True
-
-
-    return False
+    ) >= 15
+  
