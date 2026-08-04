66ca51313dc664786e910e76f02b3ef09d5b4133
guizard997-boop <guizard997@gmail.com>
2026-08-04 19:26:24 +0600

    Update car_analysis.py

--- parser/car_analysis.py
+++ parser/car_analysis.py
@@ -1,40 +1,24 @@
+
 from statistics import median
 
 
 def get_car_key(title):
-
-    """
-    Получаем ключ машины:
-    марка + модель + год
-    """
-
     words = title.lower().split()
-
     return " ".join(words[:3])
-
 
 
 def find_similar_cars(target, cars):
 
     result = []
 
-    target_key = get_car_key(
-        target["title"]
-    )
-
+    key = get_car_key(target["title"])
 
     for car in cars:
-
-        if get_car_key(car["title"]) == target_key:
-
+        if get_car_key(car["title"]) == key:
             if car["price"] > 0:
-                result.append(
-                    car["price"]
-                )
-
+                result.append(car["price"])
 
     return result
-
 
 
 def calculate_market_price(prices):
@@ -42,9 +26,7 @@ def calculate_market_price(prices):
     if len(prices) < 5:
         return 0
 
-
     return median(prices)
-
 
 
 def discount(price, market):
@@ -52,8 +34,7 @@ def discount(price, market):
     if market == 0:
         return 0
 
-
     return round(
-        ((market-price)/market)*100,
+        ((market - price) / market) * 100,
         1
     )
